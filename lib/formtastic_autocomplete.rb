59cb940b420955513ca431b90a0aec348d45eec4
Simon Protheroe <srp@square-enix.com>
2010-08-03 01:12:55 +0200

    Replaced Prototype/Scriptaculous with jQuery.

--- lib/formtastic_autocomplete.rb
+++ lib/formtastic_autocomplete.rb
@@ -1,7 +1,7 @@
 module ElanDesign
   module Formtastic
     module Autocomplete
-      
+
       protected
 
       def autocomplete_input(method, options)
@@ -9,7 +9,7 @@ module ElanDesign
         association = @object.class.reflect_on_association(method)
         if [:has_many, :has_and_belongs_to_many].include?(association.macro)
           self.label(method, options_for_label(options).merge(:for => "#{@object_name}_#{method}_autocomplete")) +
-          autocomplete_existing_entries(method, options) + 
+          autocomplete_existing_entries(method, options) +
           autocomplete_multi_field(association, options)
         else
           self.label(method, options_for_label(options).merge(:for => "#{@object_name}_#{method}_autocomplete")) +
@@ -24,10 +24,10 @@ module ElanDesign
         id_stub = "selected_#{@object_name}_#{method}"
         template.content_tag(:ul, :id => id_stub, :class => 'auto_complete_selections') do
           object.send(method).map { |selection|
-            template.content_tag(:li, 
-              template.hidden_field_tag("#{@object_name}[#{method.to_s.singularize}_ids][]", selection.send(value), :id => nil) + 
-              selection.send(label) + 
-              template.link_to_function(remove_link, "$('#{id_stub}_#{selection.send(value)}').remove()"), 
+            template.content_tag(:li,
+              template.hidden_field_tag("#{@object_name}[#{method.to_s.singularize}_ids][]", selection.send(value), :id => nil) +
+              selection.send(label) +
+              template.link_to_function(remove_link, "$('#{id_stub}_#{selection.send(value)}').remove()"),
               :id => "#{id_stub}_#{selection.send(value)}"
             )
           }.join()
@@ -43,23 +43,31 @@ module ElanDesign
         method = association.name
         label = options[:label_method] || detect_label_method(object.send(method).to_a)
         value = options[:value_method] || :id
-        current_value = object.send(method).nil? ? '' : object.send(method).send(label)
-        input_name = "#{@object_name}_#{generate_association_input_name(method)}"
-        template.text_field_tag("q", current_value, :id => "#{@object_name}_#{method}_autocomplete") + 
-        hidden_field(association.options[:foreign_key]) +
-        autocomplete_indicator(method, options) + 
-        template.content_tag(:div, "", :id => "#{@object_name}_#{method}_results", :class => 'auto_complete') +
+        current_label = object.send(method).nil? ? '' : object.send(method).send(label)
+        current_value = object.send(method).nil? ? '' : object.send(method).send(value)
+        input_name = "#{@object_name}[#{generate_association_input_name(method)}]"
+        autocomplete_input_id = generate_html_id("#{method}_autocomplete")
+        hidden_id = generate_html_id("#{generate_association_input_name(method)}")
+        template.text_field_tag("ig", current_label, :id => autocomplete_input_id) +
+        template.hidden_field_tag(input_name, current_value, :id => hidden_id) +
+ #       autocomplete_indicator(method, options) +
         template.javascript_tag(<<-EOT
-  new Ajax.Autocompleter('#{@object_name}_#{method}_autocomplete', '#{@object_name}_#{method}_results', '#{options[:url]}', {
-    updateElement: function(li) {
-      $('#{input_name}').value = li.id.replace('result_', '');
-      $('#{@object_name}_#{method}_autocomplete').value = li.innerHTML;
-    }#{", 
-    indicator: '#{@object_name}_#{method}_indicator'" if options[:indicator]}
-  })
+$(function() {
+  $("##{autocomplete_input_id}").autocomplete({
+    source: "#{options[:url]}",
+    focus: function(event, ui) {
+      $("##{autocomplete_input_id}").val(ui.item.label);
+      return false;
+    },
+    select: function(event, ui) {
+      $('##{hidden_id}').val(ui.item.value);
+      $("##{autocomplete_input_id}").val(ui.item.label);
+      return false;
+    }
+  });
+});
 EOT
-  ) 
-
+  )
       end
 
       def autocomplete_multi_field(association, options)
@@ -69,14 +77,14 @@ EOT
         remove_link = options[:remove_link] || 'Remove'
 
         template.text_field_tag("q", '', :id => "#{@object_name}_#{method}_autocomplete") +
-        autocomplete_indicator(method, options) + 
+        autocomplete_indicator(method, options) +
         template.content_tag(:div, "", :id => "#{@object_name}_#{method}_results", :class => 'auto_complete') +
         template.javascript_tag(<<-EOT
   new Ajax.Autocompleter('#{@object_name}_#{method}_autocomplete', '#{@object_name}_#{method}_results', '#{options[:url]}', {
     updateElement: function(li) {
       item_id = li.id.replace('result_', '');
       li_id = 'selected_#{@object_name}_#{method}_' + item_id;
-      if($(li_id) == null) 
+      if($(li_id) == null)
       {
         list_id = 'selected_#{@object_name}_#{method}';
         input = new Element('input', {type: 'hidden', name: '#{@object_name}[#{method.to_s.singularize}_ids][]', value: item_id});
@@ -85,19 +93,20 @@ EOT
         item = new Element('li', {id: li_id}).update(input).insert(li.innerHTML).insert(remove_link);
         $(list_id).insert(item);
       }
-      else 
+      else
       {
         new Effect.Highlight(li_id)
       }
       $('#{@object_name}_#{method}_autocomplete').value = '';
-    }#{", 
+    }#{",
     indicator: '#{@object_name}_#{method}_indicator'" if options[:indicator]}
   })
   EOT
-  ) 
+  )
       end
-   
-      
+
+
     end
   end
 end
+
